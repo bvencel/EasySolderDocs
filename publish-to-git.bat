253febf1880d14e48ad9130b2848baf96d19362d
Vencel Biro <vencel@fortech.ro>
2019-07-25 13:00:02 +0300

    Deploy from local, using batch file

--- publish-to-git.bat
+++ publish-to-git.bat
@@ -3,4 +3,4 @@ REM vuepress build
 REM Stage everything, even deleted files
 git add -A
 git commit -m "Deploy from local, using batch file"
-git push --force origin master:gh-pages
+git push --force origin master
